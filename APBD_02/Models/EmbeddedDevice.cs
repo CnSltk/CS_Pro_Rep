0c00d0ed9dd8a87bd605afecfe7d9acc28f1a305
CnSltk <cansaltik1903@gmail.com>
2025-03-20 12:03:19 +0100

    embeddedDevices

---
+++ APBD_02/Models/EmbeddedDevice.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Text.RegularExpressions;{
+    public class EmbeddedDevice
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsOn { get; set; }
+        public string IpAddress { get; set; }
+        public string NetworkName { get; set; }
+
+        public void SetIpAddress(string ipAddress)
+        {
+            var regex = new Regex(@"^(\d{1,3}\.){3}\d{1,3}$");
+            if (!regex.IsMatch(ipAddress))
+                throw new ArgumentException("Invalid IP address format.");
+
+            IpAddress = ipAddress;
+        }
+
+        public void Connect()
+        {
+            if (!NetworkName.Contains("MD Ltd."))
+                throw new ConnectionException("Invalid network name.");
+
+            Console.WriteLine($"Connected to {NetworkName} with IP {IpAddress}.");
+        }
+
+        public void TurnOn()
+        {
+            Connect(); // Connecting is part of turning on the device
+            IsOn = true;
+            Console.WriteLine($"{Name} is now ON.");
+        }
+
+        public void TurnOff()
+        {
+            IsOn = false;
+            Console.WriteLine($"{Name} is now OFF.");
+        }
+
+        public override string ToString()
+        {
+            return $"Embedded Device: {Name}, IP: {IpAddress}, Network: {NetworkName}, IsOn: {IsOn}";
+        }
+    }
+}
